97a864ccfeb59774306e8c9ba89efd2bb198bd7c
sibi <sibikozhikode@gmail.com>
2022-02-28 21:34:46 -0800

    cleared the error of darkmode when refreshing the page

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,25 @@
-console.log("all good");
+// function tog(){
+//     document.body.classList.toggle("dark-theme")
+//     if (document.getElementById('dark').checked){
+//         document.getElementById('label').innerHTML = 'Day';
+//     }else{
+//         document.getElementById('label').innerHTML = 'Night';
+//     }
+// }
+
+const body = document.body;
 function tog(){
-    document.body.classList.toggle("dark-theme")
-    if (document.getElementById('dark').checked){
-        document.getElementById('label').innerHTML = 'Day';
-    }else{
+    if (body.classList.contains('dark-theme')){
+        body.classList.remove('dark-theme');
+        localStorage.setItem("theme","light");
         document.getElementById('label').innerHTML = 'Night';
+    }else{
+        body.classList.add('dark-theme');
+        localStorage.setItem("theme","dark-theme");
+        document.getElementById('label').innerHTML = 'Day';
     }
 }
+if (localStorage.getItem("theme") === "dark-theme"){
+        body.classList.add("dark-theme");
+        document.getElementById('label').innerHTML = 'Day';
+    }
