a10ed7ce0a187fce1b1542a9ba38596e82449653
sibi <sibikozhikode@gmail.com>
2022-06-23 23:42:55 -0700

    commit for heroku

--- accounts/views.py
+++ accounts/views.py
@@ -42,6 +42,16 @@ def registration(request):
         last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password1']
+        password2 = request.POST['password2']
+        if password != password2:
+            messages.info(request,"Password not matching")
+            data ={
+                'username':username,
+                'first_name':first_name,
+                'last_name':last_name,
+                'email':email,
+            }
+            return render(request,'registration.html',data)
         try:
             user = User.objects.create_user(
                 username = username,
